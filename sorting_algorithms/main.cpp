2f05c4a0675daa4ae537c75e1369eea623df156d
Omer Shalev <omershalev@users.noreply.github.com>
2017-07-23 00:15:53 +0300

    test3: commit from laptop + removing pro.user file

--- sorting_algorithms/main.cpp
+++ sorting_algorithms/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Hello World! This is the sorting_algorithms project!" << endl;
     cout << "test1" << endl;
     cout << "test2" << endl;
+    cout << "test3" << endl;
     return 0;
 }
