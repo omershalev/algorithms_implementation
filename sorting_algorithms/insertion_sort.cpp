643d20d18a2b1140c82ba0826d3594ba84c9614c
Omer Shalev <omershalev@users.noreply.github.com>
2017-08-02 23:03:27 +0300

    Added a recursive implementation of insertion sort

--- sorting_algorithms/insertion_sort.cpp
+++ sorting_algorithms/insertion_sort.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-void insertion_sort(int arr[], int size) {
+void InsertionSort(int arr[], int size) {
     for (int i = 1; i < size; i++) {
         int key = arr[i];
         int j = i-1;
@@ -9,5 +9,23 @@ void insertion_sort(int arr[], int size) {
             j--;
         }
         arr[j+1] = key;
+    }
+}
+
+void PutInPlace(int arr[], int size, int elem) {
+    for (int i = size-1; i > 0; i--) {
+        if (elem > arr[i-1]) {
+            arr[i] = elem;
+            return;
+        } else {
+            arr[i] = arr[i-1];
+        }
+    }
+}
+
+void InsertionSortRecursive(int arr[], int size) {
+    if (size > 1) {
+        InsertionSortRecursive(arr, size-1);
+        PutInPlace(arr, size, arr[size-1]);
     }
 }
